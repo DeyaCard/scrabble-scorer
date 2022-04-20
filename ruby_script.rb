d986d5002503e3f081d41a4be5246cd94a05c2c2
Deya Card <thedeyacard@gmail.com>
2022-04-19 20:37:30 -0700

    add script
    Co-authored-by: Tony Froccaro <tfroccaro@gmail.com>
    Co-authored-by: Todd Pangilinan <pang.todd@gmail.com>
    Co-authored-by: Deya Card <thedeyacard@gmail.com>

--- ruby_script.rb
+++ ruby_script.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/ruby
 
 require ('./lib/scrabble_scorer.rb')
-
+# require_relative ('scrabble_scorer')
+puts 'Enter a word to receive its scrabble score!'
+test = Scrabble_scorer.new
+user_word = gets.chomp
+puts "your score is #{test.split
